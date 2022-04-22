b7f53f227458e31e4f1eb196570fbe60e7867474
Adeleke <kamade123@gmail.com>
2022-04-22 20:30:21 +0100

    loadFilesSync implemented, folder architecture restructured

---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    id: 'redshoe',
+    description: 'Red Shoe',
+    price: 42.12
+  }, 
+  {
+    id: 'blue jean',
+    description: 'Blue Jean',
+    price: 55.55
+  }
+];
